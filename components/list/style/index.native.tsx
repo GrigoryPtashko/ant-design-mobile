216da8a5ae2c6b603bb3d3ff552e7889083bb1dd
Grigory Ptashko <grigory.ptashko@gmail.com>
2018-04-01 14:44:46 +0300

    In List.Item removed column style from the Extra and replaced Text with View for extra; hanged color to grayscale for checkbox

--- components/list/style/index.native.tsx
+++ components/list/style/index.native.tsx
@@ -63,8 +63,6 @@ export default {
     textAlignVertical: 'center',
   },
   Extra: {
-    textAlign: 'right',
-    textAlignVertical: 'center',
   },
   Brief: {
     minHeight: variables.font_size_icontext,
